c6a92363272b21f2cc8fc4617433506aa0b0699b
Alexander <alexbui831@gmail.com>
2022-11-02 18:52:29 -0500

    Add project files.

---
+++ birthdayApplication/Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace birthdayApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.labelFName = new System.Windows.Forms.Label();
+            this.labelBirthday = new System.Windows.Forms.Label();
+            this.textBoxFName = new System.Windows.Forms.TextBox();
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.labelLName = new System.Windows.Forms.Label();
+            this.textBoxLName = new System.Windows.Forms.TextBox();
+            this.BirthdayDataGrid = new System.Windows.Forms.DataGridView();
+            this.buttonInsert = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.title2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.BirthdayDataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Wide Latin", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.SteelBlue;
+            this.label1.Location = new System.Drawing.Point(72, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 19);
+            this.label1.TabIndex = 2;
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Font = new System.Drawing.Font("Stencil", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitle.ForeColor = System.Drawing.Color.DarkOrchid;
+            this.labelTitle.Location = new System.Drawing.Point(12, -1);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(558, 63);
+            this.labelTitle.TabIndex = 3;
+            this.labelTitle.Text = "Birthday Tracker";
+            // 
+            // labelFName
+            // 
+            this.labelFName.AutoSize = true;
+            this.labelFName.Font = new System.Drawing.Font("Stencil", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelFName.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.labelFName.Location = new System.Drawing.Point(12, 78);
+            this.labelFName.Name = "labelFName";
+            this.labelFName.Size = new System.Drawing.Size(130, 22);
+            this.labelFName.TabIndex = 5;
+            this.labelFName.Text = "First Name";
+            // 
+            // labelBirthday
+            // 
+            this.labelBirthday.AutoSize = true;
+            this.labelBirthday.Font = new System.Drawing.Font("Stencil", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelBirthday.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.labelBirthday.Location = new System.Drawing.Point(460, 78);
+            this.labelBirthday.Name = "labelBirthday";
+            this.labelBirthday.Size = new System.Drawing.Size(112, 22);
+            this.labelBirthday.TabIndex = 6;
+            this.labelBirthday.Text = "Birthday";
+            // 
+            // textBoxFName
+            // 
+            this.textBoxFName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxFName.Font = new System.Drawing.Font("Cambria", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxFName.Location = new System.Drawing.Point(16, 98);
+            this.textBoxFName.Name = "textBoxFName";
+            this.textBoxFName.Size = new System.Drawing.Size(214, 20);
+            this.textBoxFName.TabIndex = 1;
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.CalendarFont = new System.Drawing.Font("Stencil", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker.CalendarMonthBackground = System.Drawing.SystemColors.MenuHighlight;
+            this.dateTimePicker.CalendarTitleForeColor = System.Drawing.SystemColors.ControlText;
+            this.dateTimePicker.CustomFormat = "MMMM - dd - yyyy";
+            this.dateTimePicker.Font = new System.Drawing.Font("Stencil", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker.Location = new System.Drawing.Point(464, 98);
+            this.dateTimePicker.Margin = new System.Windows.Forms.Padding(0);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(214, 21);
+            this.dateTimePicker.TabIndex = 3;
+            this.dateTimePicker.Value = new System.DateTime(2000, 1, 1, 0, 0, 0, 0);
+            // 
+            // labelLName
+            // 
+            this.labelLName.AutoSize = true;
+            this.labelLName.Font = new System.Drawing.Font("Stencil", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLName.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.labelLName.Location = new System.Drawing.Point(236, 78);
+            this.labelLName.Name = "labelLName";
+            this.labelLName.Size = new System.Drawing.Size(119, 22);
+            this.labelLName.TabIndex = 10;
+            this.labelLName.Text = "Last Name";
+            // 
+            // textBoxLName
+            // 
+            this.textBoxLName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxLName.Font = new System.Drawing.Font("Cambria", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxLName.Location = new System.Drawing.Point(240, 98);
+            this.textBoxLName.Name = "textBoxLName";
+            this.textBoxLName.Size = new System.Drawing.Size(214, 20);
+            this.textBoxLName.TabIndex = 2;
+            // 
+            // BirthdayDataGrid
+            // 
+            this.BirthdayDataGrid.AllowUserToAddRows = false;
+            this.BirthdayDataGrid.AllowUserToDeleteRows = false;
+            this.BirthdayDataGrid.AllowUserToResizeColumns = false;
+            this.BirthdayDataGrid.AllowUserToResizeRows = false;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            this.BirthdayDataGrid.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.BirthdayDataGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.BirthdayDataGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.BirthdayDataGrid.BackgroundColor = System.Drawing.Color.Snow;
+            this.BirthdayDataGrid.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.RaisedHorizontal;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.ScrollBar;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Stencil", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.DodgerBlue;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.ScrollBar;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.BirthdayDataGrid.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.BirthdayDataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.BirthdayDataGrid.DefaultCellStyle = dataGridViewCellStyle3;
+            this.BirthdayDataGrid.GridColor = System.Drawing.SystemColors.ControlLight;
+            this.BirthdayDataGrid.Location = new System.Drawing.Point(12, 183);
+            this.BirthdayDataGrid.Name = "BirthdayDataGrid";
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.BirthdayDataGrid.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.BirthdayDataGrid.RowHeadersVisible = false;
+            this.BirthdayDataGrid.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.BirthdayDataGrid.ShowCellErrors = false;
+            this.BirthdayDataGrid.ShowCellToolTips = false;
+            this.BirthdayDataGrid.ShowEditingIcon = false;
+            this.BirthdayDataGrid.ShowRowErrors = false;
+            this.BirthdayDataGrid.Size = new System.Drawing.Size(662, 248);
+            this.BirthdayDataGrid.TabIndex = 12;
+            // 
+            // buttonInsert
+            // 
+            this.buttonInsert.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonInsert.Font = new System.Drawing.Font("Stencil", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonInsert.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.buttonInsert.Location = new System.Drawing.Point(16, 137);
+            this.buttonInsert.Name = "buttonInsert";
+            this.buttonInsert.Size = new System.Drawing.Size(104, 40);
+            this.buttonInsert.TabIndex = 13;
+            this.buttonInsert.Text = "INSERT";
+            this.buttonInsert.UseVisualStyleBackColor = true;
+            this.buttonInsert.Click += new System.EventHandler(this.buttonInsert_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonDelete.Font = new System.Drawing.Font("Stencil", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDelete.ForeColor = System.Drawing.Color.Red;
+            this.buttonDelete.Location = new System.Drawing.Point(574, 137);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(104, 40);
+            this.buttonDelete.TabIndex = 14;
+            this.buttonDelete.Text = "DELETE";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonUpdate.Font = new System.Drawing.Font("Stencil", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonUpdate.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.buttonUpdate.Location = new System.Drawing.Point(126, 137);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(104, 40);
+            this.buttonUpdate.TabIndex = 15;
+            this.buttonUpdate.Text = "Update";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonRefresh.Font = new System.Drawing.Font("Stencil", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonRefresh.ForeColor = System.Drawing.Color.DarkOrchid;
+            this.buttonRefresh.Location = new System.Drawing.Point(464, 137);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(104, 40);
+            this.buttonRefresh.TabIndex = 16;
+            this.buttonRefresh.Text = "REFRESH";
+            this.buttonRefresh.UseVisualStyleBackColor = true;
+            this.buttonRefresh.Click += new System.EventHandler(this.buttonRefresh_Click);
+            // 
+            // title2
+            // 
+            this.title2.AutoSize = true;
+            this.title2.Font = new System.Drawing.Font("Stencil", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title2.ForeColor = System.Drawing.Color.DarkOrchid;
+            this.title2.Location = new System.Drawing.Point(20, 48);
+            this.title2.Name = "title2";
+            this.title2.Size = new System.Drawing.Size(109, 14);
+            this.title2.TabIndex = 17;
+            this.title2.Text = "Alexander Bui";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Snow;
+            this.ClientSize = new System.Drawing.Size(690, 443);
+            this.Controls.Add(this.title2);
+            this.Controls.Add(this.buttonRefresh);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonInsert);
+            this.Controls.Add(this.BirthdayDataGrid);
+            this.Controls.Add(this.textBoxLName);
+            this.Controls.Add(this.labelLName);
+            this.Controls.Add(this.dateTimePicker);
+            this.Controls.Add(this.textBoxFName);
+            this.Controls.Add(this.labelBirthday);
+            this.Controls.Add(this.labelFName);
+            this.Controls.Add(this.labelTitle);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Personal Birthday Tracker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.BirthdayDataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Label labelFName;
+        private System.Windows.Forms.Label labelBirthday;
+        private System.Windows.Forms.TextBox textBoxFName;
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.Label labelLName;
+        private System.Windows.Forms.TextBox textBoxLName;
+        private System.Windows.Forms.DataGridView BirthdayDataGrid;
+        private System.Windows.Forms.Button buttonInsert;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Button buttonRefresh;
+        private System.Windows.Forms.Label title2;
+    }
+}
+
---
+++ birthdayApplication/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace birthdayApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBoxFName.Select();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Data Source: name of server on which database resides
+            // Initial Catalog: specifies the name of database
+
+
+            // sqlcommand is a class used to perofrm read/write to database
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string connectString = @"data source=SUPERBYTE\SQLEXPRESS;initial catalog=dbBirthday;trusted_connection=true";
+            SqlConnection cnn = new SqlConnection(connectString);
+            cnn.Open();
+
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(
+                "SELECT firstName as 'First Name', lastName as 'Last Name', birthday as Birthday " +
+                "FROM users " +
+                "ORDER BY (MONTH(birthday) - MONTH(GETDATE()) + 12) % 12, DATEADD(year, YEAR(GETDATE()) - YEAR(birthday), birthday), YEAR(birthday);", cnn);
+
+            DataTable dtbl = new DataTable();
+            sqlDataAdapter.Fill(dtbl);
+
+            BirthdayDataGrid.DataSource = dtbl;
+            cnn.Close();
+        }
+
+        private void buttonInsert_Click(object sender, EventArgs e)
+        {
+            string connectString = @"data source=SUPERBYTE\SQLEXPRESS;initial catalog=dbBirthday;trusted_connection=true";
+            SqlConnection cnn = new SqlConnection(connectString);
+            cnn.Open();
+
+            SqlCommand cmd = new SqlCommand(
+                "SELECT COUNT(*) " +
+                "FROM users " +
+                "WHERE firstName LIKE '%" + textBoxFName.Text + "%' AND lastName LIKE '%" + textBoxLName.Text + "%'", cnn);
+            SqlDataReader myreader;
+
+            myreader = cmd.ExecuteReader();
+            myreader.Read();
+
+            string value = myreader[0].ToString();
+
+            myreader.Close();
+            if ((Int32.Parse(value) == 0) && (textBoxFName.Text != "" && textBoxLName.Text != ""))
+            {
+                SqlCommand sqlINSERT = new SqlCommand(
+                    "INSERT INTO users (firstName, lastName, birthday) VALUES (@firstName, @lastName, @birthday)", cnn);
+
+                sqlINSERT.Parameters.AddWithValue("@firstName", textBoxFName.Text);
+                sqlINSERT.Parameters.AddWithValue("@lastName", textBoxLName.Text);
+                sqlINSERT.Parameters.AddWithValue("@birthday", dateTimePicker.Value);
+
+                sqlINSERT.ExecuteNonQuery();
+
+                MessageBox.Show("Successful");
+
+                cnn.Close();
+                }
+            else
+            {
+                MessageBox.Show("Issue");
+            }
+            
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            string connectString = @"data source=SUPERBYTE\SQLEXPRESS;initial catalog=dbBirthday;trusted_connection=true";
+            SqlConnection cnn = new SqlConnection(connectString);
+            cnn.Open();
+
+            SqlCommand cmd = new SqlCommand(
+                "SELECT COUNT(*) " +
+                "FROM users " +
+                "WHERE firstName LIKE '%" + textBoxFName.Text + "%' AND lastName LIKE '%" + textBoxLName.Text + "%'", cnn);
+            SqlDataReader myreader;
+
+            myreader = cmd.ExecuteReader();
+            myreader.Read();
+
+            string value = myreader[0].ToString();
+
+            myreader.Close();
+
+            // Int32 converts string into integer
+            // insures updating of only 1 record at a time
+            if (Int32.Parse(value) == 1)
+            {
+
+                SqlCommand sqlUPDATE = new SqlCommand(
+                    "UPDATE users SET birthday = '" + dateTimePicker.Value + "' " +
+                    "WHERE firstName  LIKE '%" + textBoxFName.Text + "%' AND lastName LIKE '%" + textBoxLName.Text + "%'", cnn);
+                sqlUPDATE.ExecuteNonQuery();
+
+                MessageBox.Show("Update successful.");
+            } 
+            else
+            {
+                MessageBox.Show("Name not distinct.");
+            }
+    
+            cnn.Close();
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            string connectString = @"data source=SUPERBYTE\SQLEXPRESS;initial catalog=dbBirthday;trusted_connection=true";
+            SqlConnection cnn = new SqlConnection(connectString);
+            cnn.Open();
+
+            SqlCommand cmd = new SqlCommand(
+                "SELECT COUNT(*) " +
+                "FROM users " +
+                "WHERE firstName LIKE '%" + textBoxFName.Text + "%' AND lastName LIKE '%" + textBoxLName.Text + "%'", cnn);
+            SqlDataReader myreader;
+
+            myreader = cmd.ExecuteReader();
+            myreader.Read();
+
+            string value = myreader[0].ToString();
+
+            myreader.Close();
+
+            // insures deletion of only 1 record at a time
+            if ((Int32.Parse(value) == 1) && (textBoxFName.Text != "" && textBoxLName.Text != ""))
+            {
+
+                SqlCommand sqlDELETE = new SqlCommand(
+                    "DELETE FROM users " +
+                    "WHERE firstName = '" + textBoxFName.Text + "' AND lastName ='" + textBoxLName.Text + "'", cnn);
+
+                sqlDELETE.ExecuteNonQuery();
+
+                MessageBox.Show("Deleted user success.");
+            }
+            else
+            {
+                MessageBox.Show("Name not distinct.");
+            }
+
+            cnn.Close();
+        }
+
+        private void buttonRefresh_Click(object sender, EventArgs e)
+        {
+            this.Controls.Clear();
+            this.InitializeComponent();
+
+            string connectString = @"data source=SUPERBYTE\SQLEXPRESS;initial catalog=dbBirthday;trusted_connection=true";
+            SqlConnection cnn = new SqlConnection(connectString);
+            cnn.Open();
+
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(
+                "SELECT firstName as 'First Name', lastName as 'Last Name', birthday as Birthday " +
+                "FROM users " +
+                "ORDER BY (MONTH(birthday) - MONTH(GETDATE()) + 12) % 12, DATEADD(year, YEAR(GETDATE()) - YEAR(birthday), birthday), YEAR(birthday)", cnn);
+
+            DataTable dtbl = new DataTable();
+            sqlDataAdapter.Fill(dtbl);
+
+            BirthdayDataGrid.DataSource = dtbl;
+            cnn.Close();
+        }
+    }
+}
